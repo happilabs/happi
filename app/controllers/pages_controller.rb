3a04cff83e4f2001dd9c0a7a845f83ab5488e5dc
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-06-26 13:25:12 -0500

    Added voices mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,8 @@ class PagesController < ApplicationController
     @message = Message.new(params[:message])
 
     if @message.valid?
-      VoicesMailer.new_message(@message).deliver
+      email = PagesMailer.new_message(@message)
+      email.deliver
       redirect_to(root_path, :notice => "Message was successfully sent.")
     else
       flash.now.alert = "Please fill all fields."
