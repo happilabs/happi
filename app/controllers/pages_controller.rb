36a4b2239d713e4130ae6bda88619c1932155a97
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-06-26 11:32:17 -0500

    first iteration of voices mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,20 @@ class PagesController < ApplicationController
   end
 
   def voices
-
+    @message = Message.new
   end
+
+  def create_voice
+    @message = Message.new(params[:message])
+
+    if @message.valid?
+      VoicesMailer.new_message(@message).deliver
+      redirect_to(root_path, :notice => "Message was successfully sent.")
+    else
+      flash.now.alert = "Please fill all fields."
+      render :new
+    end
+  end
+
 
 end
---
+++ app/mailers/voices_mailer.rb
@@ -0,0 +1,11 @@
+class VoicesMailer < ActionMailer::Base
+
+  default :from => "noreply@youdomain.dev"
+  default :to => "you@youremail.dev"
+
+  def new_message(message)
+    @message = message
+    # mail(:subject => "[YourWebsite.tld] #{message.subject}")
+  end
+
+end
