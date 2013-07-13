d32e27c7106b9e1cd5df9f1c9855e9f58973f6f3
happilabs <happilabs@gmail.com>
2013-07-12 19:31:05 -0500

    comments out the attr_accessible in voices

--- app/models/voice.rb
+++ app/models/voice.rb
@@ -5,7 +5,7 @@ class Voice
   attribute :email
   attribute :content
 
-  attr_accessible :name, :email, :content
+  # attr_accessible :name, :email, :content
 
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
