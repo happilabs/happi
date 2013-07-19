467d91775594a1b2767653ab5ea43b9e53916b7d
stephen-puiszis <spuiszis@gmail.com>
2013-07-18 20:28:33 -0500

    adds model associations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,6 @@
 class Organization < ActiveRecord::Base
   attr_accessible :dept, :org_name
+
+  has_many :users
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :distributor_name, :manufacturer_name, :photo, :price, :product_desc, :product_name, :product_num, :recommended, :url
+
+  belongs_to :category
+  has_many :reviews
+
 end
