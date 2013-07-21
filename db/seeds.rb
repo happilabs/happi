83613cdd2597ff9401682b4907888a260e8c7d02
stephen-puiszis <spuiszis@gmail.com>
2013-07-21 16:59:11 -0500

    adds links and controller actions for the categories resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,27 @@
 
 
 Report.destroy_all
+Category.destroy_all
 
 reports = Report.create([{title: "May 2013", summary: "Summary of the May 2013 Happilabs product report", link: "may_2013_report.pdf"},
                         {title: "March 2013", summary: "Summary of the March 2013 Happilabs product report", link: "march_2013_report.pdf"}])
 
 puts "There are now #{Report.all.count} reports in the database."
+
+categories = Category.create([
+  {name: "1.5 mL Microtube" },
+  {name: "15 mL Conical Tube" },
+  {name: "50 mL Conical Tube" },
+  {name: "Nitrile Gloves" },
+  {name: "Latex Gloves" },
+  {name: "5 mL Serological Pipet" },
+  {name: "10 mL Serological Pipet" },
+  {name: "T75 Flask" },
+  {name: "10 uL Pipette Tips" },
+  {name: "200 uL Pipette Tips" },
+  {name: "100/1250 uL Pipette Tips" },
+  {name: "1.5 mL Microtube" },
+  {name: "1.5 mL Microtube" },
+  ])
+
+puts "There are now #{Category.all.count} reports in the database."
