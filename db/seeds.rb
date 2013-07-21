cbeb26afbda25813e135316af32ff3f57453cbf3
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-07-21 17:23:23 -0500

    fake seed data for reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 
 Report.destroy_all
 Category.destroy_all
+Review.destroy_all
 
 reports = Report.create([{title: "May 2013", summary: "Summary of the May 2013 Happilabs product report", link: "may_2013_report.pdf"},
                         {title: "March 2013", summary: "Summary of the March 2013 Happilabs product report", link: "march_2013_report.pdf"}])
@@ -31,4 +32,14 @@ categories = Category.create([
   {name: "1.5 mL Microtube" },
   ])
 
-puts "There are now #{Category.all.count} reports in the database."
+puts "There are now #{Category.all.count} categories in the database."
+
+reviews = Review.create([
+  {id: 1, user_id: 1, admin_approved: true, make_anonymous: false, user_recommended: false, rating: 4, desc: "Craft beer mixtape umami keffiyeh butcher. Retro Truffaut 3 wolf moon tousled beard, shabby chic typewriter Tonx before they sold out brunch. Butcher freegan skateboard Banksy forage actually tousled. Craft beer scenester lo-fi cliche. Whatever vegan YOLO craft beer, mixtape messenger bag flannel pitchfork tote bag keffiyeh." , product_id: 1},
+  {id: 2, user_id: 1, admin_approved: true, make_anonymous: true, user_recommended: true, rating: 4, desc: "Craft beer mixtape umami keffiyeh butcher. Retro Truffaut 3 wolf moon tousled beard, shabby chic typewriter Tonx before they sold out brunch. Butcher freegan skateboard Banksy forage actually tousled. Craft beer scenester lo-fi cliche. Whatever vegan YOLO craft beer, mixtape messenger bag flannel pitchfork tote bag keffiyeh." , product_id: 2},
+  {id: 3, user_id: 2, admin_approved: true, make_anonymous: false, user_recommended: true, rating: 3, desc: "Craft beer mixtape umami keffiyeh butcher. Retro Truffaut 3 wolf moon tousled beard, shabby chic typewriter Tonx before they sold out brunch. Butcher freegan skateboard Banksy forage actually tousled. Craft beer scenester lo-fi cliche. Whatever vegan YOLO craft beer, mixtape messenger bag flannel pitchfork tote bag keffiyeh." , product_id: 2},
+  {id: 4, user_id: 2, admin_approved: true, make_anonymous: false, user_recommended: true, rating: 5, desc: "Seitan put a bird on it photo booth VHS. Fixie DIY High Life trust fund master cleanse. Try-hard synth authentic, viral leggings brunch cornhole polaroid Williamsburg bicycle rights literally put a bird on it. American apparel yr squid gentrify asymmetrical pug. ", product_id: 3},
+  {id: 5, user_id: 1, admin_approved: false, make_anonymous: false, user_recommended: true, rating: 2, desc: "Mlkshk pitchfork fingerstache PBR. Portland master cleanse kitsch, tattooed fanny pack cliche chambray leggings pug. 3 wolf moon twee farm-to-table stumptown kitsch iPhone, mustache Vice 90's semiotics gentrify. Aesthetic Williamsburg try-hard, whatever 3 wolf moon single-origin coffee narwhal Bushwick YOLO tote bag", product_id: 4}
+  ])
+
+puts "There are now #{Review.all.count} reviews in the database."
