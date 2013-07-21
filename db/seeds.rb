b7fda489bf53883e219368e895c8622a9c1022b0
stephen-puiszis <spuiszis@gmail.com>
2013-07-21 17:41:49 -0500

    edits category views

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,8 +27,6 @@ categories = Category.create([
   {name: "10 uL Pipette Tips" },
   {name: "200 uL Pipette Tips" },
   {name: "100/1250 uL Pipette Tips" },
-  {name: "1.5 mL Microtube" },
-  {name: "1.5 mL Microtube" },
   ])
 
 puts "There are now #{Category.all.count} reports in the database."
