adf2c73e07b6ba603605f83151c656cfa09d19ed
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-07-26 18:48:08 -0500

    adds validations for not logged in users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
-  before_filter :validate_admin, :except => [:index, :show]
+  before_filter :validate_admin, :except => [:show]
+  before_filter :authenticate_user!, :only => [:show]
 
   # GET /products
   # GET /products.json
