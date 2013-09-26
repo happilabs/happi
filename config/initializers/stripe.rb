f1341415944c79c377f35c19d342a7034fd3fd70
happilabs <happilabs@gmail.com>
2013-09-25 20:45:39 -0500

    swap Stripe api keys test to live

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => "pk_test_9PskHUu23luHHqu6pwVYSDEt",
-  :secret_key      => "sk_test_2xGwezZVNIAO3x7LtFr0kk8S"
+  :publishable_key => "pk_live_n1ljYhjnayiqJYmCVqRGYCiB",
+  :secret_key      => "sk_live_ymd3OmzBg67gdd1UHEgVrV2F"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
