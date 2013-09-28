2d36d0d3bf8393cee31061daa22db0b5551ccb24
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-28 15:22:32 -0500

    clean up product sort method

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,11 +4,8 @@ class Category < ActiveRecord::Base
   has_many :products
 
   def products_sorted_by_average_product_rating
-    sorted_array = []
-    product_array = self.products.map! do |product|
-      product
-    end
-
+    product_array = self.products.map! { |product| product }
     product_array.sort_by {|product| product.reviews.average("rating").to_f}.reverse
   end
+
 end
